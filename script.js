3e033315e4bb4aa59575e7218bc7b5b1b595f746
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-30 17:00:54 -0400

    Add capability to change grid dimension

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
-function colorSquare(e) {
+function getSquareSize(dim) {
+/*
+    Given the dimension of the grid, find the size of the squares to fit the width of the container
+*/
+    const container = document.querySelector('.container');
+    containerWidth = container.offsetWidth;
+    return `${containerWidth / dim}px`;
+}
+
+function colorSquare() {
     this.classList.add('visited');
 }
 
 // Create dim x dim grid with squares that leaved colored trails as the mouse hovers over them
-let dim = 16;
-
-for(let row = 0; row < dim; row++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('row');
-
-    for(let col = 0; col < dim; col++) {
-        const square = document.createElement('div');
-        // Have the square change color as the mouse hovers over them
-        square.addEventListener('mouseenter', colorSquare);
-        rowContainer.appendChild(square);
+function drawGrid(dim=16) {
+    const squareSize = getSquareSize(dim);
+    for(let row = 0; row < dim; row++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row');
+    
+        for(let col = 0; col < dim; col++) {
+            const square = document.createElement('div');
+            square.style.width = squareSize;
+            square.style.height = squareSize;
+            // Have the square change color as the mouse hovers over them
+            square.addEventListener('mouseenter', colorSquare);
+            rowContainer.appendChild(square);
+        }
+    
+        const container = document.querySelector('.container');
+        container.appendChild(rowContainer);
     }
-
-    document.body.appendChild(rowContainer);
 }
+
+drawGrid(); // Initialize default grid
+
+/*
+const btn = document.querySelector('button');
+btn.addEventListener('click', )
+*/
